ad33b8cac67ddfc085e840cb56bb6739a9dc769d
BOOK-5TV8UAHDVO\kim_j <jong1kim2000@gmail.com>
2025-05-07 20:37:51 +0900

    수정

--- fastapi-app/tests/test_main.py
+++ fastapi-app/tests/test_main.py
@@ -1,4 +1,8 @@
 # test_main.py
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import json
 import pytest
 from fastapi.testclient import TestClient
